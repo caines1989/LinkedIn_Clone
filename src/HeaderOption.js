7faca1ba04628c589e264938f6210676ce6957eb
caines1989 <6423872+caines1989@users.noreply.github.com>
2021-08-15 12:09:11 +0100

    line 12 edit

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -9,7 +9,7 @@ function HeaderOption({ avatar, Icon, title, onClick}) {
     return (
         <div onClick={onClick} className='headerOption'>
             {Icon && <Icon className='headerOption__icon' />}
-            {avatar && ( <Avatar className='headerOption__icon'>{user?.email[0]}</Avatar>)}
+            {avatar && ( <Avatar className='headerOption__icon'>{user && user.email[0]}</Avatar>)}
             
             <h3 className='headerOption__title'>{title}</h3>
         </div>
